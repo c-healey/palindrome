ef6b3d42ebb1360ccef009e0c3cef65f776a49a0
Cathy <cathy.healey@gmail.com>
2020-04-17 11:19:40 -0400

    Updates

---
+++ precache-manifest.b601b575a1fdaed1ebb5a1e018172fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02fe705f31e1f56584249d45595ea0e",
+    "url": "/palindrome/index.html"
+  },
+  {
+    "revision": "502d72e55d52ef15ea80",
+    "url": "/palindrome/static/css/main.c0853681.chunk.css"
+  },
+  {
+    "revision": "1921d40c6b2a1ade7230",
+    "url": "/palindrome/static/js/2.50fd2a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/palindrome/static/js/2.50fd2a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502d72e55d52ef15ea80",
+    "url": "/palindrome/static/js/main.9dfc1f70.chunk.js"
+  },
+  {
+    "revision": "e3d7bf63454e20a59840",
+    "url": "/palindrome/static/js/runtime-main.9a50c721.js"
+  }
+]);
